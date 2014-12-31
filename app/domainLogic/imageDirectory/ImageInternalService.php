43603bc1b2deac44e4851a470403492d58f40b00
Kevin Washington <kevw12188@gmail.com>
2014-12-31 11:34:52 -0500

    some prep for next session

--- app/domainLogic/imageDirectory/ImageInternalService.php
+++ app/domainLogic/imageDirectory/ImageInternalService.php
@@ -15,17 +15,18 @@ class ImageInternalService {
     {
         //get image file - not done
 
-        //determine if uploaded file was image - not done
+        //determine if uploaded file was image - DONE
 
         //validate that its in png format - DONE
 
-        //name the image - not done
+        //create four sizes (small, medium, and large, original) - not done
 
-        //create three sizes (small, medium, and large) - not done
+        //create image object
+            //attach the paths (small, medium, large, original) to image object - not done
+            //name the image object - not done
+            //store the image in the database - not done
 
-        //attach the paths (small, medium, large, original) to image object - not done
-
-        //store the image in the database - not done
+        //return the image object?
     }
 
     public function show()
