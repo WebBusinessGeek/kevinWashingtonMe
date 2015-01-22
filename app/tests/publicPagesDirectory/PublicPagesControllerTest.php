6a2c2442b6dc539b5ff4ee50e236c356124fbe10
Kevin Washington <kevw12188@gmail.com>
2015-01-22 11:43:59 -0500

    added test cases for later implementation to publicPagesController

--- app/tests/publicPagesDirectory/PublicPagesControllerTest.php
+++ app/tests/publicPagesDirectory/PublicPagesControllerTest.php
@@ -15,6 +15,274 @@ class PublicPagesControllerTest extends \App\Base\MasterTestLibrary {
     }
 
 
+    /***********************************************************************************************************/
+    /*                                          View Test Cases                                              */
+    /***********************************************************************************************************/
 
+    /**
+     * @group publicPagesControllerTests
+     * @group publicPagesViewTests
+     * @group publicPagesControllerHomeTests
+     */
+    public function test_viewHome_route_is_setup()
+    {
+
+    }
+
+    /**
+     * @group publicPagesControllerTests
+     * @group publicPagesViewTests
+     * @group publicPagesControllerSkillsTests
+     */
+    public function test_viewSkills_route_is_setup()
+    {
+
+    }
+
+    /**
+     * @group publicPagesControllerTests
+     * @group publicPagesViewTests
+     * @group publicPagesControllerExperiencesTests
+     */
+    public function test_viewExperiences_route_is_setup()
+    {
+
+    }
+
+    /**
+     * @group publicPagesControllerTests
+     * @group publicPagesViewTests
+     * @group publicPagesControllerConnectTests
+     */
+    public function test_viewConnect_route_is_setup()
+    {
+
+    }
+
+    /**
+     * @group publicPagesControllerTests
+     * @group publicPagesViewTests
+     * @group publicPagesControllerHomeTests
+     */
+    public function test_viewHome_view_exists()
+    {
+
+    }
+
+    /**
+     * @group publicPagesControllerTests
+     * @group publicPagesViewTests
+     * @group publicPagesControllerSkillsTests
+     */
+    public function test_viewSkills_view_exists()
+    {
+
+    }
+
+    /**
+     * @group publicPagesControllerTests
+     * @group publicPagesViewTests
+     * @group publicPagesControllerExperiencesTests
+     */
+    public function test_viewExperiences_view_exists()
+    {
+
+    }
+
+    /**
+     * @group publicPagesControllerTests
+     * @group publicPagesViewTests
+     * @group publicPagesControllerConnectTests
+     */
+    public function test_viewConnect_view_exists()
+    {
+
+    }
+
+    /**
+     * @group publicPagesControllerTests
+     * @group publicPagesViewTests
+     * @group publicPagesControllerHomeTests
+     */
+    public function test_viewHome_correct_view_returned()
+    {
+
+    }
+
+    /**
+     * @group publicPagesControllerTests
+     * @group publicPagesViewTests
+     * @group publicPagesControllerSkillsTests
+     */
+    public function test_viewSkills_correct_view_returned()
+    {
+
+    }
+
+    /**
+     * @group publicPagesControllerTests
+     * @group publicPagesViewTests
+     * @group publicPagesControllerExperiencesTests
+     */
+    public function test_viewExperiences_correct_view_returned()
+    {
+
+    }
+
+    /**
+     * @group publicPagesControllerTests
+     * @group publicPagesViewTests
+     * @group publicPagesControllerConnectTests
+     */
+    public function test_viewConnect_correct_view_returned()
+    {
+
+    }
+
+
+
+
+
+    /***********************************************************************************************************/
+    /*                                          Get Data Test Cases                                              */
+    /***********************************************************************************************************/
+
+
+    /**
+     * @group publicPagesControllerTests
+     * @group publicPagesControllerHomeTests
+     * @group publicPagesGetDataTests
+     */
+    public function test_getDataHome_entities_needed_for_view_are_returned()
+    {
+
+    }
+
+    /**
+     * @group publicPagesControllerTests
+     * @group publicPagesControllerHomeTests
+     * @group publicPagesGetDataTests
+     */
+    public function test_getDataHome_entities_are_accessible()
+    {
+
+    }
+
+    /**
+     * @group publicPagesControllerTests
+     * @group publicPagesControllerHomeTests
+     * @group publicPagesGetDataTests
+     */
+    public function test_getDataHome_entity_relationships_are_accessible()
+    {
+
+    }
+
+    /**
+     * @group publicPagesControllerTests
+     * @group publicPagesControllerSkillsTests
+     * @group publicPagesGetDataTests
+     */
+    public function test_getDataSkills_entities_needed_for_view_are_returned()
+    {
+
+    }
+
+    /**
+     * @group publicPagesControllerTests
+     * @group publicPagesControllerSkillsTests
+     * @group publicPagesGetDataTests
+     */
+    public function test_getDataSkills_entities_are_accessible()
+    {
+
+    }
+
+    /**
+     * @group publicPagesControllerTests
+     * @group publicPagesControllerSkillsTests
+     * @group publicPagesGetDataTests
+     */
+    public function test_getDataSkills_entity_relationships_are_accessible()
+    {
+
+    }
+
+    /**
+     * @group publicPagesControllerTests
+     * @group publicPagesControllerExperiencesTests
+     * @group publicPagesGetDataTests
+     */
+    public function test_getDataExperiences_entities_needed_for_view_are_returned()
+    {
+
+    }
+
+    /**
+     * @group publicPagesControllerTests
+     * @group publicPagesControllerExperiencesTests
+     * @group publicPagesGetDataTests
+     */
+    public function test_getDataExperiences_entities_are_accessible()
+    {
+
+    }
+
+    /**
+     * @group publicPagesControllerTests
+     * @group publicPagesControllerExperiencesTests
+     * @group publicPagesGetDataTests
+     */
+    public function test_getDataExperiences_entity_relationships_are_accessible()
+    {
+
+    }
+
+
+
+
+    /***********************************************************************************************************/
+    /*                                          Post Data Test Cases                                              */
+    /***********************************************************************************************************/
+
+    /**
+     * @group publicPagesControllerTests
+     * @group publicPagesControllerConnectTests
+     * @group publicPagesPostDataTests
+     */
+    public function test_postDataConnect_returns_correct_view_on_success()
+    {
+
+    }
+
+    /**
+     * @group publicPagesControllerTests
+     * @group publicPagesControllerConnectTests
+     * @group publicPagesPostDataTests
+     */
+    public function test_postDataConnect_returns_correct_message_on_success()
+    {
+
+    }
+
+    /**
+     * @group publicPagesControllerTests
+     * @group publicPagesControllerConnectTests
+     * @group publicPagesPostDataTests
+     */
+    public function test_postDataConnect_returns_correct_view_on_attribute_error()
+    {
+
+    }
+
+    /**
+     * @group publicPagesControllerTests
+     * @group publicPagesControllerConnectTests
+     * @group publicPagesPostDataTests
+     */
+    public function test_postDataConnect_returns_correct_message_on_bad_attributes_error()
+    {
+
+    }
 
 }
