f4d00501cf29bdbaeb115ea3327f4916b2ca950e
Kevin Washington <kevw12188@gmail.com>
2015-01-23 17:34:11 -0500

    revised some of the nav bar

--- app/views/publicPages/wrapper.php
+++ app/views/publicPages/wrapper.php
@@ -23,7 +23,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="/">KevinWashington.me Skeleton</a>
+            <a class="navbar-brand" href="/">KevinWashington.Me</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
