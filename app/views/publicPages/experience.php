12aa64dcca1bd49581f74743393837239ad0911b
Kevin Washington <kevw12188@gmail.com>
2015-01-23 14:33:00 -0500

    completed experience show template

--- app/views/publicPages/experience.php
+++ app/views/publicPages/experience.php
@@ -21,21 +21,112 @@
 
         <div id="highlights" class="col-md-7 col-sm-offset-1 well">
             <h4>Highlights</h4>
-            <p>Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.</p>
-            <p>Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.</p>
-            <p>Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.</p>
-            <p>Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.</p>
-            <p>Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.</p>
-            <p>Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.</p>
-            <p>Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.</p>
-            <p>Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.</p>
+            <ul>
+                <li>Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.</li>
+                <li>Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.</li>
+                <li>Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.</li>
+                <li>Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.</li>
+                <li>Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.</li>
+                <li>Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.</li>
+                <li>Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.</li>
+                <li>Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.</li>
+                <li>Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.</li>
+                <li>Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.</li>
+                <li>Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.</li>
+
+            </ul>
+        </div>
+    </div>
+
+    <div class="row">
+        <div id="rolesResponsibilities" class="col-md-12 well">
+            <h4>Description of project</h4>
+            <p> Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.
+            Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.
+            Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.
+            Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.
+            Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.
+            Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.
+            Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.
+            Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly. </p>
+
+            <br/>
+            <h4>Roles and Responsiblities</h4>
+            <ul>
+                <li>Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.</li>
+                <li>Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.</li>
+                <li>Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.</li>
+                <li>Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.</li>
+                <li>Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.</li>
+                <li>Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.</li>
+                <li>Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.</li>
+                <li>Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.</li>
+                <li>Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.</li>
+                <li>Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.</li>
+            </ul>
 
         </div>
     </div>
 
-    <div id="rolesResponsibilities">
-    </div>
 
-    <div id="interview">
+    <div class="row">
+        <div id="interview" class="col-md-12 well">
+            <h4 class="text-center">Feedback From the Client</h4>
+
+            <div class="questionGroup">
+                    <h4>Q: A Question that needs an answer</h4>
+                    <div class="row">
+                        <div class="col-sm-1">
+                            <img src="http://placehold.it/150x150">
+                        </div>
+                        <div class="col-sm-10 col-lg-offset-1">
+                        <p> Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.
+                            Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.
+                            Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.
+                            Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.
+                            Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.
+                            Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.
+                            Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.
+                            Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly. </p>
+                        </div>
+                    </div>
+            </div>
+            <div class="questionGroup">
+                <h4>Q: A Question that needs an answer</h4>
+                <div class="row">
+                    <div class="col-sm-1">
+                        <img src="http://placehold.it/150x150">
+                    </div>
+                    <div class="col-sm-10 col-lg-offset-1">
+                        <p> Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.
+                            Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.
+                            Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.
+                            Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.
+                            Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.
+                            Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.
+                            Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.
+                            Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly. </p>
+                    </div>
+                </div>
+            </div>
+            <div class="questionGroup">
+                <h4>Q: A Question that needs an answer</h4>
+                <div class="row">
+                    <div class="col-sm-1">
+                        <img src="http://placehold.it/150x150">
+                    </div>
+                    <div class="col-sm-10 col-lg-offset-1">
+                        <p> Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.
+                            Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.
+                            Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.
+                            Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.
+                            Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.
+                            Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.
+                            Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.
+                            Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly. </p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
