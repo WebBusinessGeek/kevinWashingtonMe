32ec956b6172a4e807ab9c404a71514be912394b
Kevin Washington <kevw12188@gmail.com>
2015-01-23 14:13:34 -0500

    working on experiences template, pushing due to machine instability and to prevent loss of work

---
+++ app/views/publicPages/experience.php
@@ -0,0 +1,41 @@
+<div id="experienceIndex">
+
+</div>
+
+
+
+
+
+
+
+<div id="experienceShow">
+
+    <div class="row text-center">
+        <div id="companyClientInfo" class="col-md-4 well">
+            <h4>Company/Client Info</h4>
+            <img src="http://placehold.it/150x150">
+            <p><b>Some info: </b>Info about it. </p>
+            <p><b>Some info: </b>Info about it. </p>
+            <p><b>Some info: </b>Info about it. </p>
+        </div>
+
+        <div id="highlights" class="col-md-7 col-sm-offset-1 well">
+            <h4>Highlights</h4>
+            <p>Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.</p>
+            <p>Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.</p>
+            <p>Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.</p>
+            <p>Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.</p>
+            <p>Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.</p>
+            <p>Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.</p>
+            <p>Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.</p>
+            <p>Chupa chups unerdwear.com liquorice tiramisu marshmallow marzipan jelly.</p>
+
+        </div>
+    </div>
+
+    <div id="rolesResponsibilities">
+    </div>
+
+    <div id="interview">
+    </div>
+</div>
