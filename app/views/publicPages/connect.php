5aa5a35daa6cdc47b5a5e0aabd47a8e3cb398749
Kevin Washington <kevw12188@gmail.com>
2015-01-23 19:16:18 -0500

    working on connect template, committing to prevent loss of work

--- app/views/publicPages/connect.php
+++ app/views/publicPages/connect.php
@@ -13,3 +13,45 @@
     </div>
 
 </div>
+
+
+<div id="contactForm" class="col-md-8 col-sm-offset-1 well">
+    <div id="contactFormHeader" class="text-center">
+        <h4>Simple Contact Form</h4>
+        <p>A quick way to get in touch with me. When happy with your message just hit send.</p>
+    </div>
+
+    <div id="contactFormInputs" class="col-md-5 well">
+        <form class="form-horizontal">
+            <div class="form-group">
+                <label for="inputEmail3" class="col-sm-5 control-label">Help me understand how I could help your business objectives.</label>
+                <div class="col-sm-5">
+                    <textarea class="form-control" rows="3"></textarea>
+                </div>
+            </div>
+<!--        inputs form-->
+    </div>
+    <div id="contactFormPreview" class="col-md-6 col-sm-offset-1 well">
+        <p>Preview TextPreview TextPreview TextPreview TextPreview Text
+            Preview TextPreview TextPreview TextPreview TextPreview TextPreview Text
+            Preview TextPreview TextPreview TextPreview TextPreview TextPreview Text
+            Preview TextPreview TextPreview TextPreview TextPreview Text
+            Preview TextPreview TextPreview TextPreview TextPreview Text
+            Preview TextPreview TextPreview TextPreview Text</p>
+    </div>
+</div>
+
+
+<div>
+<!--    email section-->
+</div>
+
+
+<div>
+<!--    skype section-->
+</div>
+
+
+<div>
+<!--    phone section-->
+</div>
