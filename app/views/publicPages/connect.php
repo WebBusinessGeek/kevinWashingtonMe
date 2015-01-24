b15f5ac9cd04352f145d30e95f09b2987e1efb75
Kevin Washington <kevw12188@gmail.com>
2015-01-23 19:00:55 -0500

    added connect template

---
+++ app/views/publicPages/connect.php
@@ -0,0 +1,15 @@
+<div id="contactNav" class="col-md-3 well">
+    <div id="contactNavHeader">
+        <h4>4 Easy Ways to Connect</h4>
+    </div>
+
+    <div id="contactNavList">
+        <ol>
+            <li><a hfref="#">Simple Contact Form</a></li>
+            <li><a hfref="#">Email Me</a></li>
+            <li><a hfref="#">Add Me on Skype</a></li>
+            <li><a hfref="#">Leave Me a Voicemail</a></li>
+        </ol>
+    </div>
+
+</div>
