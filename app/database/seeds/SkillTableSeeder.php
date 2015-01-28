2607048117aa09b83b031f04ebc16a4857b9dc05
Kevin Washington <kevw12188@gmail.com>
2015-01-28 17:26:20 -0500

    working on skill seed file, committing to prevent loss of work

--- app/database/seeds/SkillTableSeeder.php
+++ app/database/seeds/SkillTableSeeder.php
@@ -10,7 +10,48 @@ use Faker\Factory as Faker;
 
 class SkillTableSeeder extends \Illuminate\Database\Seeder {
 
+    protected $categoryCount = [
+        1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18
+    ];
 
+    protected $skillTitleGroups = [
+        1 => [
+            'Customer Research','Market Research','Competitor Research'
+        ],
+
+        2=> [
+            'UML Modeling','App Entity Identification','Software Architecture','Branding','Revenue Generation Strategy',
+            'Beach Head Market Selection','Beach Head Market Penetration Tactics','Follow On Market Selection','Marketing Strategy'
+        ],
+
+        3 => [
+            'Laravel Development','AngularJS Component Development','Angular on Laravel','Twitter Bootstrap','PHP',
+            'HTML','CSS3','jQuery','Professional Coding','OOP','Database Architecture','Rails Project Management',
+            'Software Project Management','Scrum Project Management'
+        ],
+
+        4 => [
+            'Email Content Creation','Email Marketing Strategy','Email Marketing Campaign Management',
+            'Email Marketing Campaign Optimization','Creative List Building'
+        ],
+
+        5 => [
+            'Link Opportunity Research','Link Quality Research','Linkable Asset Development','Linkable Asset Strategy'
+        ],
+
+        6 => [
+            'Content Marketing Strategy','Funnel Development','Content Creation','Content Marketing Success Audit','Content Marketing Optimization',
+            'Content Promotion'
+        ],
+
+        7 => [
+            'Keyword Research','OnSite Markup Optimization','Niche Blog Development','Keyword Selection','SERP Competition Research', 'Vendor Qualification'
+        ],
+
+        8 => [
+            'Paid Advertising Campaign Strategy','Paid Advertising Campaign Audit','Paid Advertising Campaign Optimization'
+        ]
+    ];
     public function run()
     {
         \Illuminate\Support\Facades\DB::table('skills')->truncate();
