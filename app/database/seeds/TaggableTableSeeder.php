04e79b50812bb1dfdec109ed110f2bfc4dec6ef7
Kevin Washington <kevw12188@gmail.com>
2015-01-29 11:02:00 -0500

    completed tag table seeder

--- app/database/seeds/TaggableTableSeeder.php
+++ app/database/seeds/TaggableTableSeeder.php
@@ -9,13 +9,13 @@
 class TaggableTableSeeder extends \Illuminate\Database\Seeder {
 
 
-//    protected $tagIds =[
-//        1,2,3,4,5,6,7,8,9,10,
-//        11,12,13,14,15,16,17,18,19,20,
-//        21,22,23,24,25,26,27,28,29,30,
-//        31,32,33,34,35
-//    ];
-//
+    protected $tagIds =[
+        1,2,3,4,5,6,7,8,9,10,
+        11,12,13,14,15,16,17,18,19,20,
+        21,22,23,24,25,26,27,28,29,30,
+        31,32,33,34,35
+    ];
+
     protected $tagSkillRelationships= [
 
         1 => [1,2,3,32,33,42,46,47,62,81,97],
@@ -52,27 +52,60 @@ class TaggableTableSeeder extends \Illuminate\Database\Seeder {
 
         17 => [36,37,38,39,40,41],
 
-        18 => [48,49,50]
+        18 => [48,49,50],
 
+        19 => [42, 45, 46],
+
+        20 => [56, 57, 58],
+
+        21 => [51,52,53,54,55],
+
+        22 => [59, 60, 61, 62, 63, 64, 65, 66],
+
+        23 => [84,85,86,87,91,92,94,95,96,98,99,100,101,102,103,104,105,106],
+
+        24 => [24,25,26],
+
+        25 => [24,25,26],
+
+        26 => [6,7,8,9,10,11,12,27,34,35,37,37,52,56,57,58,78,79,83],
+
+        27 => [16,19,38,56],
+
+        28 => [75,84,85,91,94,95,96,98,99,100,101,102,104,105],
+
+        29 => [56,57,58],
+
+        30 => [75,84,85,91,94,95,96,98,99,100,101,102,104,105],
+
+        31 => [51, 52, 53, 54, 55, 89, 90, 92, 93, 105, 106],
+
+        32 => [51, 52, 53, 54, 55, 89, 90, 92, 93, 105, 106],
+
+        33 => [51,52,53,54,55],
+
+        34 => [31],
+
+        35 => [51, 52, 53, 54, 55, 89, 90, 92, 93, 105, 106],
     ];
-//
-//    public function run()
-//    {
-//        \Illuminate\Support\Facades\DB::table('taggables')->truncate();
-//
-//
-//        foreach($this->tagIds as $tagId)
-//        {
-//            $tag = \App\DomainLogic\TagDirectory\Tag::find($tagId);
-//
-//            foreach($this->tagSkillRelationships[$tagId] as $skillId)
-//            {
-//                $tag->skills()->attach($skillId);
-//            }
-//        }
-//
-//
-//    }
+
+    public function run()
+    {
+        \Illuminate\Support\Facades\DB::table('taggables')->truncate();
+
+
+        foreach($this->tagIds as $tagId)
+        {
+            $tag = \App\DomainLogic\TagDirectory\Tag::find($tagId);
+
+            foreach($this->tagSkillRelationships[$tagId] as $skillId)
+            {
+                $tag->skills()->attach($skillId);
+            }
+        }
+
+
+    }
 
 //    public function run()
 //    {
