00db221786fcc5bb40f91243f67e1173f20d4869
Kevin Washington <kevw12188@gmail.com>
2015-01-28 20:28:05 -0500

    some prep for taggable table seeder implementation

--- app/database/seeds/TaggableTableSeeder.php
+++ app/database/seeds/TaggableTableSeeder.php
@@ -8,6 +8,36 @@
 
 class TaggableTableSeeder extends \Illuminate\Database\Seeder {
 
+   //for each tag - attach its skills
+
+//    protected $tagIds =[
+//        1,2,3,4,5,6,7,8,9,10,
+//        11,12,13,14,15,16,17,18,19,20,
+//        21,22,23,24,25,26,27,28,29,30,
+//        31,32,33,34,35
+//    ];
+//
+//    protected $tagSkillRelationships= [
+//
+//    ];
+//
+//    public function run()
+//    {
+//        \Illuminate\Support\Facades\DB::table('taggables')->truncate();
+//
+//
+//        foreach($this->tagIds as $tagId)
+//        {
+//            $tag = \App\DomainLogic\TagDirectory\Tag::find($tagId);
+//
+//            foreach($this->tagSkillRelationships[$tagId] as $skillId)
+//            {
+//                $tag->skills()->attach($skillId);
+//            }
+//        }
+//
+//
+//    }
 
     public function run()
     {
