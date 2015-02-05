f81573f7ae1ec73288fcc22b18c3dc62e101d49d
Kevin Washington <kevw12188@gmail.com>
2015-02-05 11:59:46 -0500

    organized cover stylings into customCover.css file

--- app/views/publicPages/cover.php
+++ app/views/publicPages/cover.php
@@ -9,130 +9,16 @@
 
     <link href="/angular-bootstrap/bootstrap-twit/css/bootstrap-theme.min.css" rel="stylesheet">
     <link href="/angular-bootstrap/bootstrap-twit/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/angular-bootstrap/bootstrap-twit/css/customCover.css" rel="stylesheet">
+
     <link rel="shortcut icon" href="/assets/kwFavIcon.ico" type="image/x-icon">
     <link rel="icon" href="/assets/kwFavIcon.ico" type="image/x-icon">
+
+    
     <link href='http://fonts.googleapis.com/css?family=Buenard:700' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Arvo:400,700|Roboto:100' rel='stylesheet' type='text/css'>
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-
-    <style>
-        body{
-            background: transparent url('http://lorempixel.com/1500/800/business/');
-            /*background-size: cover;*/
-        }
-
-        .headline {
-            font-family: 'Arvo', serif;
-            font-size:  64px;
-        }
-        .subheadline {
-            font-family: 'Roboto', sans-serif;
-            font-size: 34px;
-        }
-
-        .btn-huge-cta {
-            min-height: 60px;
-            min-width: 205px;
-            padding-top: 15px;
-            text-align: center;
-            font-size: 18px;
-            font-family: 'Roboto', sans-serif;
-
-        }
-
-        .cta-image {
-            margin-left:35px;
-            margin-top:20px;
-        }
-
-
-        .element-to-move {
-            -webkit-animation: myfirst 1s; /* Chrome, Safari, Opera */
-            animation: myfirst 1s;
-            -webkit-animation-iteration-count: infinite;
-            animation-iteration-count: infinite;
-            animation-direction: alternate;
-            -webkit-animation-direction: alternate;
-        }
-
-        /* Chrome, Safari, Opera */
-        @-webkit-keyframes myfirst {
-            from {margin-top: 10px;}
-            to {margin-top: 30px;}
-        }
-
-        /* Standard syntax */
-        @keyframes myfirst {
-            from {margin-top: 10px;}
-            to {margin-top: 30px;}
-        }
-
-        .brandImage{
-            position:absolute;
-            top:22px;
-            left:50px;
-        }
-
-
-        .icon-bar{
-            background-color: #fff;
-
-        }
-
-        .navbar-static-top {
-            margin-top: -100px;
-           padding-top: 35px;
-            padding-right: 50px;
-        }
-
-        .navLinks {
-            color: #fff;
-            font-family: 'Arvo', serif;
-            font-size: 20px;
-            margin-right: 30px;
-        }
-
-        .navLinks:hover {
-            color: #000;
-        }
-
-
-        .navbar-collapse {
-            margin-left:100px;
-        }
-
-        #video-bg {
-            position: fixed;
-            right: 0;
-            bottom: 0;
-            width: auto;
-            min-width: 100%;
-            height: auto;
-            min-height: 100%;
-            z-index: -100;
-        }
-
-
-
-       .video-section h1{
-           color:#fff;
-       }
-       .video-section p{
-           color:#fff;
-       }
-
-        .video-section .pattern-overlay {
-            background-color: rgba(71, 71, 71, 0.59);
-            padding: 110px 0 32px;
-            min-height: 715px;
-        }
-
-
-
-
-
-
-    </style>
 
 </head>
 <body>
