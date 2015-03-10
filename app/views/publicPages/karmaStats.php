dcf7be3bea42c6a7968374a0799a765a35d4674e
Kevin Washington <kevw12188@gmail.com>
2015-03-10 16:37:25 -0400

    beginnings of karmaStatsLanding page design on site

--- app/views/publicPages/karmaStats.php
+++ app/views/publicPages/karmaStats.php
@@ -1,25 +1,47 @@
 <div>
-    <div id="section1" class="col-sm-12 col-md-12 col-lg-12 backgroundImageUse">
-        <div>
-            <h1>Any strategist can tell you what they can do...</h1>
+    <div id="section1" class="col-sm-12 col-md-12 col-lg-12">
+        <div class="row">
+            <div id="slide1_textSection1" class="col-sm-10 col-md-10 col-lg-10">
+                <h1>Any strategist can <em class="underline">tell</em> you what they can do...</h1>
+            </div>
         </div>
-    </div>
-    <div id="section2" class="col-sm-12 col-md-12 col-lg-12 backgroundImageUse">
+        <div class="row">
+            <div id="slide1_personTalkingImage" class="col-sm-8 col-md-8 col-lg-8 col-lg-offset-5">
+            </div>
+        </div>
+        <div class="row">
+            <div id="slide1_analyticAbstract" class="col-sm-6 col-md-6 col-lg-6">
+            </div>
+<!--        </div>-->
+<!--        <div class="row">-->
+            <div id="slide1_textSection2" class="col-sm-6 col-md-6 col-lg-6">
+                <h1>Let me <em class="underline">show</em> you what I've done.</h1>
+                <p>
+                    Here's what karmaCRM and I were able to accomplish in just 5 months
+                    when we focused ont he smartest objectievs and implemented creative tactics
+                    to achieve success!
+                </p>
+            </div>
+        </div>
+
 
     </div>
-    <div id="section3" class="col-sm-12 col-md-12 col-lg-12 backgroundImageUse">
+    <div id="section2" class="col-sm-12 col-md-12 col-lg-12">
 
     </div>
-    <div id="section4" class="col-sm-12 col-md-12 col-lg-12 backgroundImageUse">
+    <div id="section3" class="col-sm-12 col-md-12 col-lg-12">
 
     </div>
-    <div id="section5" class="col-sm-12 col-md-12 col-lg-12 backgroundImageUse">
+    <div id="section4" class="col-sm-12 col-md-12 col-lg-12">
 
     </div>
-    <div id="section6" class="col-sm-12 col-md-12 col-lg-12 backgroundImageUse">
+    <div id="section5" class="col-sm-12 col-md-12 col-lg-12">
 
     </div>
-    <div id="section7" class="col-sm-12 col-md-12 col-lg-12 backgroundImageUse">
+    <div id="section6" class="col-sm-12 col-md-12 col-lg-12">
+
+    </div>
+    <div id="section7" class="col-sm-12 col-md-12 col-lg-12">
 
     </div>
 </div>
