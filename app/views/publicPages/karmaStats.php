cbb07d5785334f9f05e71989ea2b0b8c3bca3c2c
Kevin Washington <kevw12188@gmail.com>
2015-03-11 09:52:18 -0400

    finished slide 5 on karma stats landinge page

--- app/views/publicPages/karmaStats.php
+++ app/views/publicPages/karmaStats.php
@@ -26,8 +26,10 @@
                 </div>
             </div>
         </div>
-
     </div>
+
+
+
 
     <div id="section2" class="col-sm-12 col-md-12 col-lg-12">
         <div class="row">
@@ -67,15 +69,11 @@
                 <h2>When we optimized the sales process we had a notable increase in conversions...</h2>
             </div>
         </div>
-
-
         <div class="row">
             <div id="section3_analyticGear" class="col-lg-6">
                 <img class="img-responsive" src="/assets/karmaStatsImages/KWStats_slide3_analyticCircle.png">
             </div>
         </div>
-
-
         <div class="row">
             <div id="section3_textSection2" class="col-lg-7">
                 <h1>A 145% increase to be exact!</h1>
@@ -87,18 +85,17 @@
                 </p>
             </div>
         </div>
-
-
         <div class="row">
             <div id="section3_analyticChart" class="col-lg-6 col-lg-offset-7">
                 <img class="img-responsive" src="/assets/karmaStatsImages/KWStats_slide3_analyticChart.png">
             </div>
         </div>
-
-
     </div>
-    <div id="section4" class="col-sm-12 col-md-12 col-lg-12">
 
+
+
+
+    <div id="section4" class="col-sm-12 col-md-12 col-lg-12">
         <div class="row">
             <div id="slide4_textSection1" class="col-lg-9 text-center">
                 <h2>With our new strategy we increased revenue by 449%!</h2>
@@ -106,7 +103,6 @@
                     While increasing our conversions was a great start, we also implemented tactics that collectively help initiate an enormous spike
                     in the amount of money the business collected on a daily basis. These tactics included a overhaul on
                     our pricing options to incentivize the plans that would help us achieve the objectives most important to us.
-
                 </p>
             </div>
         </div>
@@ -115,7 +111,6 @@
                 <img class="img-responsive" src="/assets/karmaStatsImages/KWStats_slide4_piggyBank.png">
             </div>
         </div>
-
         <div class="row">
             <div id="slide4_laptop" class="col-lg-12 text-center">
                 <img class="img-responsive center-block" src="/assets/karmaStatsImages/KWStats_slide4_laptop.png">
@@ -127,9 +122,39 @@
             </div>
         </div>
     </div>
-    <div id="section5" class="col-sm-12 col-md-12 col-lg-12">
 
+
+
+    <div id="section5" class="col-sm-12 col-md-12 col-lg-12">
+        <div class="row">
+            <div id="slide5_textSection1" class="col-lg-9 col-lg-offset-1 text-center">
+                <h2>And lastly we made more revenue stick by 1079%!</h2>
+                <p>
+                    Yup, you read correctly. We increased the amount of revenue we retained after attrition factors such as cancellations and card expirys by 1,079%!
+                    We did this not only by bringing in more revenue, but also launching a few tactics that directly combated many of the factors causing us to
+                    lose money in the back end.
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div id="slide5_percentCircle" class="col-lg-4 col-lg-offset-8">
+                <img class="img-responsive" src="/assets/karmaStatsImages/KWStats_slide5_percentCircle.png">
+            </div>
+        </div>
+        <div class="row">
+            <div id="slide5_balanceSheet" class="col-lg-12 text-center">
+                <img class="img-responsive center-block" src="/assets/karmaStatsImages/KWStats_slide5_balanceSheet.png">
+            </div>
+        </div>
+        <div class="row">
+            <div id="slide5_analyticGear" class="col-lg-4">
+                <img class="img-responsive" src="/assets/karmaStatsImages/KWStats_slide5_analyticGear.png">
+            </div>
+        </div>
     </div>
+
+
+
     <div id="section6" class="col-sm-12 col-md-12 col-lg-12">
 
     </div>
