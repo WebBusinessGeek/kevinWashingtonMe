c25236418344d07fd1db96331aede4a0c1ff1cee
Kevin Washington <kevw12188@gmail.com>
2015-04-15 13:48:51 -0400

    revised intro content

--- app/views/publicPages/intro.php
+++ app/views/publicPages/intro.php
@@ -13,20 +13,27 @@
                 <p class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 col-sm-8 col-sm-offset-2">I not only help businesses create innovative products and grow their customer base - i enjoy it.</p>
 
                 <p class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 col-sm-8 col-sm-offset-2">
-                    Im a self taught web developer with extensive working experience in core development technologies like PHP, Javascript, HTML, CSS, and SQL.
-                    I also have worked with these techonologies in popular frameworks like Angular, Laravel, and Twitter Bootstrap
+                    Im a self taught web developer with working experience in core development technologies like PHP, Javascript, HTML, CSS, and SQL.
+                    I have also worked extensively with these technologies in well known frameworks like Angular, Laravel, and Twitter Bootstrap.
                 </p>
 
                 <p class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 col-sm-8 col-sm-offset-2">
-                    In addition to my web development skills  have a great deal of tacit experience in customer acquisition.
-                    I understand the processes involved in architecting, building, and optimizing effective acquisition channels.
-                    I also understand how to get target markets into thses channels using popular traction meiums like SEO, Content Marketing, and Paid Advertising.
-                    While also identifying unique traction opportunities like Direct Communication, and building Taget Market Web applications.
+                    In addition to web development I have a great deal of tacit experience in customer acquisition.
+                </p>
+                <p class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 col-sm-8 col-sm-offset-2">
+                    I understand the processes involved in architecting, building, and optimizing effective acquisition channels
+                    and have experience funneling prospects and targets into these channels using a combination of traction mediums.
+                </p>
+                <p class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 col-sm-8 col-sm-offset-2">
+                    These mediums include well known methods like SEO, Content Marketing, and Paid Advertising -
+                    and innovative opportunities like Direct Communication Marketing, and Custom Web Applications.
                 </p>
 
                 <p class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 col-sm-8 col-sm-offset-2">
                     I'm always looking for opportunities to work with talented people on cool projects, learn something new, and get paid in the process.
-                    If you know of any such opportunities please feel free to contact me.
+                </p>
+                <p class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 col-sm-8 col-sm-offset-2">
+                    If you know of any such opportunities feel free to <a href="/connect">contact me</a>.
                 </p>
             </div>
             <div id="introContentSig">
