6557bdf856af049eaa086428a91556cf90d6f29b
Kevin Washington <kevw12188@gmail.com>
2015-04-17 16:53:41 -0400

    moved google analytics script to head

--- app/views/publicPages/cover.php
+++ app/views/publicPages/cover.php
@@ -19,6 +19,17 @@
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+        ga('create', 'UA-59821417-1', 'auto');
+        ga('send', 'pageview');
+    </script>
+
+    <script src="//static.getclicky.com/js" type="text/javascript"></script>
+    <script type="text/javascript">try{ clicky.init(100818102); }catch(e){}</script>
 </head>
 <body>
 
@@ -71,16 +82,6 @@
 
 <script src="/angular-bootstrap/bootstrap-twit/js/bootstrap.min.js"></script>
 
-<script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-    ga('create', 'UA-59821417-1', 'auto');
-    ga('send', 'pageview');
-</script>
 
-<script src="//static.getclicky.com/js" type="text/javascript"></script>
-<script type="text/javascript">try{ clicky.init(100818102); }catch(e){}</script>
 </body>
 </html>
