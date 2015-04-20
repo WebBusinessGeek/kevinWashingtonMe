89b74baa9cb9ad2be73b41eb02623caccef3348a
Kevin Washington <kevw12188@gmail.com>
2015-04-20 14:18:50 -0400

    revised the newInquiry email template by making it use the blade templating engine

---
+++ app/views/emails/newInquiry.blade.php
@@ -0,0 +1,12 @@
+<div>
+
+    <h1>New Inquiry</h1>
+
+        <p>Email: {{potentialInquiry->email}}</p>
+        <p>Phone: {{potentialInquiry->phone}}</p>
+        <p>Message: {{potentialInquiry->body}}</p>
+        <p>Preferred Contact Method: {{potentialInquiry->contactMethod}}</p>
+        
+
+
+</div>
--- app/views/emails/newInquiry.php
+++
@@ -1,9 +0,0 @@
-<div>
-    <h2>New Inquiry</h2>
-    <p>
-        <?php
-        echo $potentialInquiry->email;
-        ?>
-        ?>
-    </p>
-</div>
