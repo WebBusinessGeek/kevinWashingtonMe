15c8f8ef991eb36a6f379057d237d05a2ffb0f1b
Kevin Washington <kevw12188@gmail.com>
2015-04-20 14:23:25 -0400

    added name to template

--- app/views/emails/newInquiry.blade.php
+++ app/views/emails/newInquiry.blade.php
@@ -3,6 +3,7 @@
 
     <h1>New Inquiry</h1>
 
+        <p>Name: {{$potentialInquiry->name}}</p>
         <p>Email: {{$potentialInquiry->email}}</p>
         <p>Phone: {{$potentialInquiry->phone}}</p>
         <p>Message: {{$potentialInquiry->body}}</p>
