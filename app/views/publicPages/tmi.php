a055fae6ef4bcbb9a7bc13c75b9f791961925c4b
Kevin Washington <kevw12188@gmail.com>
2015-04-20 10:45:06 -0400

    added tmi section set up including ability to dynamically change the content section

--- app/views/publicPages/tmi.php
+++ app/views/publicPages/tmi.php
@@ -1,4 +1,41 @@
 
-<div ng-controller="tmiController">
-    <p>TMI page</p>
+<div class="text-center" ng-controller="tmiController">
+    <div id="tmiHeadline">
+        <h1>Some additional information</h1>
+    </div>
+
+    <div class="row text-center" id="tmiNavList">
+        <div class="imageContainer col-sm-3 col-md-3 col-lg-3"><a ng-click="show('resume')"><img src="http://placehold.it/125">resume</a></div>
+
+        <div class="imageContainer col-sm-3 col-md-3 col-lg-3"><a ng-click="show('10Things')"><img src="http://placehold.it/125">10things</a></div>
+
+        <div class="imageContainer col-sm-3 col-md-3 col-lg-3"><a ng-click="show('skills')"><img src="http://placehold.it/125">skills</a></div>
+
+        <div class="imageContainer col-sm-3 col-md-3 col-lg-3"><a ng-click="show('visualWorkHistory')"><img src="http://placehold.it/125">visual</a></div>
+    </div>
+
+
+    <div id="tmiResume" ng-show="showing == 'resume'">
+        <div>
+            <p>Resume section</p>
+        </div>
+    </div>
+
+    <div id="tmi10Things" ng-show="showing == '10Things'">
+        <div>
+            <p>10 things section</p>
+        </div>
+    </div>
+
+    <div id="tmiSkills" ng-show="showing == 'skills'">
+        <div>
+            <p>Skills section</p>
+        </div>
+    </div>
+
+    <div id="tmiVisualWorkHistory" ng-show="showing == 'visualWorkHistory'">
+        <div>
+            <p>Visual work history section</p>
+        </div>
+    </div>
 </div>
--- public/app/tmi.js
+++ public/app/tmi.js
@@ -5,4 +5,11 @@ angular.module('app')
     .controller('tmiController', ['$scope', function($scope)
     {
         $scope.$parent.currentUrl = 'tmi';
+
+        $scope.showing = '10Things';
+
+        $scope.show = function(target)
+        {
+            $scope.showing = target;
+        }
     }]);
