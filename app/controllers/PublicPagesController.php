d71832c734a80c31dc0f5f41a538e0e0044a7a0d
Kevin Washington <kevw12188@gmail.com>
2015-04-20 14:06:23 -0400

    email send working in connect page, now working on transferring data

--- app/controllers/PublicPagesController.php
+++ app/controllers/PublicPagesController.php
@@ -149,7 +149,7 @@ class PublicPagesController extends \BaseController {
 
 		if($this->inquiryService->isModelInstance($potentialInquiry))
 		{
-			Mail::send('emails.newInquiry', [], function($message)
+			Mail::send('emails.newInquiry', [$potentialInquiry], function($message)
 			{
 				$message->to('hello@kevinwashington.me')->subject('New Inquiry from Main Connect');
 			});
--- app/views/emails/newInquiry.php
+++ app/views/emails/newInquiry.php
@@ -1,5 +1,9 @@
 <div>
     <h2>New Inquiry</h2>
     <p>
+        <?php
+        echo $potentialInquiry->email;
+        ?>
+        ?>
     </p>
 </div>
