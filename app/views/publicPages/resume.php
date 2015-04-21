cec8c8b5160965b48b7cdb9d7809c98d076bfa2f
Kevin Washington <kevw12188@gmail.com>
2015-04-21 19:47:54 -0400

    content to resume page

--- app/views/publicPages/resume.php
+++ app/views/publicPages/resume.php
@@ -1,19 +1,36 @@
 
-<div class="row">
+<div class="row text-center">
     <div id="resumeIntro" class="col-lg-4 col-md-4 well">
-        <p>This is the intro section</p>
+        <div>
+            <img class="img-circle img-responsive center-block" src="http://placehold.it/200">
+            <h4><span id="firstName">Kevin</span> <span id="lastName">Washington</span></h4>
+            <h5>Web Developer & Customer Acquisition Specialist</h5>
+        </div>
+
+        <div>
+
+        </div>
     </div>
 
     <div class="col-lg-8 col-md-8 well">
         <div id="resumeCustAcq" class="col-lg-6 col-md-6 well">
-            <p>This is the cust Acq section</p>
+            <div>
+                <img src="http://placehold.it/64">
+                <h4>Customer Acquisition</h4>
+            </div>
         </div>
         <div id="resumeWebDev" class="col-lg-6 col-md-6 well">
-            <p>This is the web dev section</p>
+            <div>
+                <img src="http://placehold.it/64">
+                <h4>Web Development</h4>
+            </div>
         </div>
 
         <div id="resumeMgr" class="col-lg-12 col-md-12 well">
-            <p>This is the Manager section</p>
+            <div>
+                <img src="http://placehold.it/64">
+                <h4>Management</h4>
+            </div>
         </div>
     </div>
 
