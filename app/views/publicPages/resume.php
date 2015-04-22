48d08faca56bea6ec34c503786b6465c65cac751
Kevin Washington <kevw12188@gmail.com>
2015-04-21 20:00:03 -0400

    content for resume

--- app/views/publicPages/resume.php
+++ app/views/publicPages/resume.php
@@ -7,9 +7,6 @@
             <h5>Web Developer & Customer Acquisition Specialist</h5>
         </div>
 
-        <div>
-
-        </div>
     </div>
 
     <div class="col-lg-8 col-md-8 well">
@@ -18,11 +15,69 @@
                 <img src="http://placehold.it/64">
                 <h4>Customer Acquisition</h4>
             </div>
+
+            <div>
+                <h5>Specialties</h5>
+                <p>Developing & Optimizing Acquisition Channels</p>
+                <p>Gaining Insights Through Data</p>
+                <p>Process Optimization & Development</p>
+                <p>Growth</p>
+
+                <h5>Engagement Methods</h5>
+                <p>SEO</p>
+                <p>Social</p>
+                <p>Advertising</p>
+                <p>Direct Interactions</p>
+
+                <h5>Optimization Ready Checklist</h5>
+                <p>VWO</p>
+                <p>Google Analytics</p>
+                <p>Clicky</p>
+                <p>Creativity</p>
+
+                <h5>Quick Sales Tips</h5>
+                <ol>
+                    <li>Understand the product</li>
+                    <li>Know the User</li>
+                    <li>Engage by Adding value</li>
+                    <li>Sell Benefits not Features</li>
+                    <li>Remember to ask for the sale</li>
+                    <li>Don't forget to ask again</li>
+                </ol>
+            </div>
         </div>
         <div id="resumeWebDev" class="col-lg-6 col-md-6 well">
             <div>
                 <img src="http://placehold.it/64">
                 <h4>Web Development</h4>
+
+                <div>
+                    <h5>Core Technologies</h5>
+                    <p>PHP</p>
+                    <p>Javascript</p>
+                    <p>HTML</p>
+                    <p>CSS</p>
+                    <p>SQL</p>
+
+
+                    <h5>Frameworks</h5>
+                    <p>Laravel 4.*</p>
+                    <p>AngularJS 1.*</p>
+                    <p>jQuery</p>
+                    <p>Twitter Bootstrap</p>
+
+                    <h5>Weapons of Mass Development</h5>
+                    <p>PhotoShop</p>
+                    <p>PHPUnit</p>
+                    <p>PHPStorm</p>
+                    <p>Macbook Air & Internet Connection</p>
+
+                    <h5>My Development Mantra</h5>
+                    <p>Design for Usability</p>
+                    <p>Architect for Change</p>
+                    <p>Program for Maintenance</p>
+                    <p>Deliver in Sprints</p>
+                </div>
             </div>
         </div>
 
