d4af6618e0e84d7b9532e2aa04010f236b1c296e
Kevin Washington <kevw12188@gmail.com>
2015-05-06 11:11:07 -0400

    finalized concept layout for recruitment engagement content page

--- app/views/publicPages/employmentAcquisition/engagement.php
+++ app/views/publicPages/employmentAcquisition/engagement.php
@@ -1,4 +1,4 @@
-<div id="articleBody">
+<div id="articleBody" class="container text-center">
     <div id="articleHeader" class="text-center">
         <div id="articleTitle">
             <h1>11 Traits to Look For in Your Next Marketing Hire.</h1>
@@ -9,7 +9,59 @@
     </div>
 
 
-
+    <div id="section1">
+        <div class="contentHeaderSection row">
+            <div class="numberIcon col-lg-12">
+                <img class="img-circle img-responsive center-block" src="http://placehold.it/80">
+            </div>
+            <div class="contentHeading col-lg-12">
+                <h3>Understands The Importance of Product & User Knowledge</h3>
+            </div>
+        </div>
+        <div class="contentBodySection row">
+            <div class="contentImg col-lg-12">
+                <img class="img-circle img-responsive center-block" src="http://placehold.it/256">
+            </div>
+            <div class="contentSection col-lg-6 col-lg-offset-3">
+                <div>
+                    <h5>What is this trait?</h5>
+                    <p>
+                        This is the why Content. This is the why content. This is the why content.
+                        This is the why Content. This is the why content. This is the why content.
+                        This is the why Content. This is the why content. This is the why content.
+                        This is the why Content. This is the why content. This is the why content.
+                    </p>
+                </div>
+                <div>
+                    <h5>Benefits to the business:</h5>
+                    <p>
+                        This is the benefit Content. This is the benefit content. This is the benefit content.
+                        This is the benefit Content. This is the benefit content. This is the benefit content.
+                        This is the benefit Content. This is the benefit content. This is the benefit content.
+                        This is the benefit Content. This is the benefit content. This is the benefit content.
+                    </p>
+                </div>
+                <div>
+                    <h5>How to Spot it?</h5>
+                    <p>
+                        This is the spot it Content. This is the spot it content. This is the spot it content.
+                        This is the spot it Content. This is the spot it content. This is the spot it content.
+                        This is the spot it Content. This is the spot it content. This is the spot it content.
+                        This is the spot it Content. This is the spot it content. This is the spot it content.
+                    </p>
+                </div>
+                <div>
+                    <h5>Don't be fooled!</h5>
+                    <p>
+                        This is the fooled Content. This is the fooled content. This is the fooled content.
+                        This is the fooled Content. This is the fooled content. This is the fooled content.
+                        This is the fooled Content. This is the fooled content. This is the fooled content.
+                        This is the fooled Content. This is the fooled content. This is the fooled content.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 
