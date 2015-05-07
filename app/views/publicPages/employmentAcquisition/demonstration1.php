6dcc5d2845b43af74c710b223c7942d17d026724
Kevin Washington <kevw12188@gmail.com>
2015-05-07 13:56:01 -0400

    added content to both demo content pages

--- app/views/publicPages/employmentAcquisition/demonstration1.php
+++ app/views/publicPages/employmentAcquisition/demonstration1.php
@@ -1,1 +1,40 @@
-<div>This is the demonstration 1 </div>
+<div class="demoContentBody" ng-controller="demonstration1Controller">
+
+    <div class="demoContentHeading">
+        <h1>Am I a Good Fit for Your Team?</h1>
+    </div>
+
+    <div class="demoContentBody">
+        <p>
+            I would like to think I could be a great addition to your team.
+            However I don't know your business as well as you do.
+            So I put this together to help you come to your own conclusion.
+        </p>
+    </div>
+
+    <div class="videoOrTranscriptSection">
+        <div class="demoVideoSection">
+            <div>
+                <p>This will be the video</p>
+            </div>
+        </div>
+
+        <div class="demoTranscriptSection">
+            <div>
+                <p>This will be the transcript</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="demoCallToActionSection">
+        <a href="/recruitment/1/lets-work-together" class="btn btn-warning">Click to get there</a>
+    </div>
+
+
+
+
+
+
+
+
+</div>
