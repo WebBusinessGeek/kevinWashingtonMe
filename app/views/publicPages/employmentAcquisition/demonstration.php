841fc7978b60d8e7e29038c9930858a565e6a7fa
Kevin Washington <kevw12188@gmail.com>
2015-05-08 17:18:05 -0400

    added question/answer groups 1 2 and 3 to recruitment demonstration page for the transcript section

--- app/views/publicPages/employmentAcquisition/demonstration.php
+++ app/views/publicPages/employmentAcquisition/demonstration.php
@@ -42,8 +42,116 @@
         </div>
 
         <div ng-show="VTSetting == 'transcript'" class="demoTranscriptSection col-lg-8 col-lg-offset-2">
-            <div>
-                <p>This will be the transcript</p>
+            <div id="demoTranscriptHeader">
+                <h5>Transcript of the video:</h5>
+                <p>Hi, I'm Kevin.</p>
+                <p>Im a web developer and customer acquisition specialist focusing primarily on web based acquisition.</p>
+                <p>
+                    I put together this short video to help you decide if I could be a good addition to your team...
+                    and also as an opportunity to share what I am looking for in my next company.
+                </p>
+                <p>
+                    In order to do this I thought of 5 of the toughest interview questions I could think of, and answered them for you.
+                </p>
+                <p>
+                    Keep in mind that you cna click the button right above me to switch from the video to the transcript and quickly browse the questions and answer groups.
+                </p>
+                <p>
+                    So to keep this short video short, let's get started!
+                </p>
+            </div>
+            <div id="demoTranscriptQuestion1">
+                <h3>Question 1</h3>
+                <h4>What are you looking for, and hoping you don't find, in your next position?</h4>
+                <p>
+                    The three main things I am looking for in my next position are great leadership, good returns,
+                    and opportunities for self improvement.
+                </p>
+                <p>
+                    A good leadership team to me is one that develops meaningful objectives, gives its team members clear direction,
+                    and ensures everyone on the team (including leaders) are held accountable.
+                </p>
+                <p>
+                    For returns I just want to be paid comparatively to the value I bring back to the business.
+                    I would like to be able to grow with the business and be motivated to help make it successful.
+                </p>
+                <p>
+                    And lastly, I'm on the look out for positions where learning and self-improvement is cultivated.
+                    Whether this is through interactions with peers, or though external resources - striving to constantly
+                    improve is something I take seriously and am looking for in my next company.
+                </p>
+                <p>
+                    The things I am not looking for would be the exact opposite.
+                    I hope I can avoid companies with poor leadership,  a lack of learning opportunities, and where employees are not valued fairly.
+                </p>
+            </div>
+            <div id="demoTranscriptQuestion2">
+                <h3>Question 2</h3>
+                <h4>If you can build and sell web solutions, why haven't you build your own product?</h4>
+                <p>
+                    To be honest the reason I'm looking for a position rather than starting my own company is patience.
+                </p>
+                <p>
+                    I definitely have aspirations to build and market my own software company one day, however
+                    there are a few things I'm wary of.
+                </p>
+                <p>
+                    For one there are things that I feel I could be better at, one being polishing my front end programming skills
+                    and shortening my development time. I would also like to become a better designer as this is one of my weaker points.
+                </p>
+                <p>
+                    There are alos a few things I would like to learn before rolling out my own software based business like various security protocols
+                    and getting comfortable with penetration testing.
+                </p>
+                <p>
+                    Lastly the thing I absolutely don't want to start my company without is a large prospect pool to funnel into paying customers.
+                    A common cause of business failure isn't a bad product but the inability to find and sell new customers.
+                    I don't want to have this problem so I'm looking to build a pool of prospects until I feel I'm ready.
+                </p>
+            </div>
+            <div id="demoTranscriptQuestion3">
+                <h3>Question 3</h3>
+                <h4>How have you helped other businesses get more customers, and how do you plan to generate new business for mine?</h4>
+                <p>
+                    The last company I worked with in a growth capacity was karmaCRM.
+                    karmaCRM is a business that provides web based CRM tools to small and medium sized businesses.
+                    Shortly after taking over as the director of growth at karma I helped develop and implement a strategy that led
+                    to a 145% increase in overall conversions, a 449% increase in revenue, and a 1079% increase in daily revenue after our churn rates were factored in.
+                </p>
+                <p>
+                    While some of these increases are huge the steps we used to get there are not. And each one can be duplicated for most businesses.
+                </p>
+                <p>
+                    The process is simple.
+                </p>
+                <p>
+                    First I understand your business and how its performing by researching your lead generation and sales analytics.
+                </p>
+                <p>
+                    Second, I get a complete understanding of your product and customers.
+                </p>
+                <p>
+                    For products, I like to understand the characteristics and features, the problems the features solve, and the tasks they make easier.
+                </p>
+                <p>
+                    For your customers, I want to understand what they care about, why, and where they are located online.
+                </p>
+                <p>
+                    With an understanding of your business, your product and your customers we can develop and effectively
+                    attack the core objectives that should be our primary focus.
+                </p>
+                <p>
+                    These core objectives often include optimizing under performing sales channels, building new sales channels,
+                    and improving business analytics to increase the ROI of our marketing spend.
+                </p>
+            </div>
+            <div id="demoTranscriptQuestion4">
+                <h3>Question 4</h3>
+                <h4>What is your biggest strength and why?</h4>
+            </div>
+            <div id="demoTranscriptQuestion5">
+                <h3>Question 5</h3>
+                <h4>In your opinion, what's the three most important things in business?</h4>
             </div>
         </div>
     </div>
