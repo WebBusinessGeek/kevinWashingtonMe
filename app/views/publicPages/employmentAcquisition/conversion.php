8e945bd16fe61c3f4b89dbea14339c2210844876
Kevin Washington <kevw12188@gmail.com>
2015-05-07 20:00:56 -0400

    added some content to recruitment conversion page

--- app/views/publicPages/employmentAcquisition/conversion.php
+++ app/views/publicPages/employmentAcquisition/conversion.php
@@ -1,1 +1,22 @@
-<div>This is a conversion page</div>
+<div id="recruitmentConversionBody" class="text-center">
+
+    <div id="recruitmentConversionContentHeader">
+        <div>
+            <h1>Let's Set up an Interview!</h1>
+        </div>
+
+        <div>
+            <p>
+                I'm glad you think I could make a good addition to your team.
+            </p>
+
+            <p>
+                Please fill out the really short form below and I will get back to you (within 24 hours) with my availability.
+            </p>
+
+            <p>
+                Looking forward to learning more about your opportunity.
+            </p>
+        </div>
+    </div>
+</div>
