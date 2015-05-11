c067475e763da6a6e2d918f64e0677f7d5c3ae32
Kevin Washington <kevw12188@gmail.com>
2015-05-11 10:43:41 -0400

    added conversion content form - sections for requestor information and interview information

--- app/views/publicPages/employmentAcquisition/conversion.php
+++ app/views/publicPages/employmentAcquisition/conversion.php
@@ -11,10 +11,7 @@
                 I'm glad you think I could make a good addition to your team.
             </p>
             <p>
-                Please fill out the really short form below and I will get back to you (within 24 hours) with my availability.
-            </p>
-            <p>
-                Looking forward to learning more about your opportunity.
+                Looking forward to learning more about your opportunity!
             </p>
         </div>
 
@@ -32,31 +29,99 @@
             </div>
         </div>
         <div id="interviewFormSection" ng-show="interviewConnectSetting == 'interview'">
-            <p>This will be the interview form.</p>
+            <p>Please fill out this short form and I will plan to get back to you with my availability within 24 hours.</p>
+
+            <form>
+                <div id="requesterInformation">
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <input type="text" class="form-control" id="name" placeholder="example - Carl Winslow" ng-model="contactName">
+                    </div>
+                    <div class="form-group">
+                        <label for="contactMethod">Best Contact Method</label>
+                        <select class="form-control" id="contactMethod"  ng-model="contactMethodValue">
+                            <option value="email">Email</option>
+                            <option value="phone">Phone</option>
+                        </select>
+                    </div>
+                    <div class="form-group" ng-show="contactMethodValue == 'email'">
+                        <label for="email">Email</label>
+                        <input type="email" class="form-control" id="email" placeholder="example - carlWinslow@familymatters.com" ng-model="contactEmail">
+                    </div>
+                    <div class="form-group" ng-show="contactMethodValue == 'phone'">
+                        <label for="phone">Phone</label>
+                        <input type="text" class="form-control" id="phone" placeholder="example - 610-444-4444" ng-model="contactPhone">
+                    </div>
+                </div>
+
+                <div id="interviewInformation">
+                    <div class="form-group">
+                        <label for="interviewType">Interview Type</label>
+                        <select class="form-control" id="interviewType" ng-model="interviewTypeValue">
+                            <option value="video">Video (Skype, Google Hangouts, etc.)</option>
+                            <option value="person">In Person (Reasonably close to Philadelphia only)</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="interviewContactPersonDifferent">Interview Contact Different from above?</label>
+                        <input type="checkbox" id="interviewContactPersonDifferent" value="true"  ng-model="interviewContactPersonDifferentValue">
+                    </div>
+                    <div ng-show="interviewContactPersonDifferentValue">
+                        <div class="form-group">
+                            <label for="interviewContactPersonName">Contact Person's Name</label>
+                            <input type="text" class="form-control" id="interviewContactPersonName" ng-model="interviewContactPersonName" placeholder="example - Laura Winslow">
+                        </div>
+                        <div class="form-group">
+                            <label for="interviewContactMethod">Best Contact Method</label>
+                            <select class="form-control" id="interviewContactMethod"  ng-model="interviewContactMethodValue">
+                                <option value="email">Email</option>
+                                <option value="phone">Phone</option>
+                            </select>
+                        </div>
+                        <div class="form-group" ng-show="interviewContactMethodValue == 'email'">
+                            <label for="interviewEmail">Contact Person's Email</label>
+                            <input type="email" class="form-control" id="interviewEmail" placeholder="example - lauraWinslow@familymatters.com" ng-model="interviewContactEmail">
+                        </div>
+                        <div class="form-group" ng-show="interviewContactMethodValue == 'phone'">
+                            <label for="interviewPhone">Contact Person's Phone</label>
+                            <input type="text" class="form-control" id="interviewPhone" placeholder="example - 610-444-4444" ng-model="interviewContactPhone">
+                        </div>
+                    </div>
+
+                </div>
+
+                <div id="positionInformation">
+
+                </div>
+
+                <button type="submit" class="btn btn-default">Submit</button>
+                {{interviewContactPersonName}}
+                {{interviewContactMethodValue}}
+                {{interviewContactEmail}}
+                {{interviewContactPhone}}
+            </form>
+
             <!--
 
-            name
-            best contact method
-            contact info (phone / email)
-            message
-
             interview
-                type
-                    in-person
-                    Skype
-                contact to schedule interview
+                contact person to schedule interview
                     same as above
+                    or
                     new space for new name
                     new space for new contact method
                     new space for new contact info (phone / email)
 
             position information
+                company
                 info about the roles/ responsiblities
                 remote
                     true
                     false
                     both
-                full-time, part-time, project
+                type
+                    full time
+                    part time
+                    project
                 compensation
                     slider
                     don’t know yet
