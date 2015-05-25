7e5365b7a57d7f9e0c4744b3cc7aacc343f61974
Kevin Washington <kevw12188@gmail.com>
2015-05-25 17:19:44 -0400

    added dynamic company name and contact retrieval in reEngagement for offer content

--- app/views/publicPages/employmentAcquisition/reEngagementForOffer.php
+++ app/views/publicPages/employmentAcquisition/reEngagementForOffer.php
@@ -1,4 +1,3 @@
 <div ng-controller="reEngagementForOfferController">
-    {{tester}}
-    This is the re-engagement for the meeting.
+    {{contactName}} {{companyName}}
 </div>
--- public/app/reEngagementForOffer.js
+++ public/app/reEngagementForOffer.js
@@ -2,5 +2,16 @@
 angular.module('app')
     .controller('reEngagementForOfferController', ['$scope', function($scope)
     {
-        $scope.tester = 'this is a test to ensure angular is working';
+        $scope.identifyContactNameAndCompanyName = function()
+        {
+            $scope.url = window.location.pathname;
+            $scope.splitUrlByDashes = $scope.url.split('-');
+            $scope.getContactName = $scope.splitUrlByDashes[2];
+            $scope.getCompanyName = $scope.splitUrlByDashes[13];
+            return [$scope.getContactName, $scope.getCompanyName];
+        };
+
+        $scope.contactName = $scope.identifyContactNameAndCompanyName()[0];
+        $scope.companyName = $scope.identifyContactNameAndCompanyName()[1];
+
     }]);
