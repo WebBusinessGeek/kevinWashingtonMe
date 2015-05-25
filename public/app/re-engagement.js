f6a0e59d093f90a63345dc83015b0ddd86ade0b8
Kevin Washington <kevw12188@gmail.com>
2015-05-25 16:26:37 -0400

    jsut cleaned up the reengagement.js file a bit

--- public/app/re-engagement.js
+++ public/app/re-engagement.js
@@ -3,9 +3,6 @@ angular.module('app')
 
     .controller('reengagementController', ['$scope', function($scope)
     {
-
-        $scope.tester = 'some test to ensure angular is working';
-
         $scope.identifyCompanyName = function()
         {
             $scope.getUrl = window.location.pathname;
