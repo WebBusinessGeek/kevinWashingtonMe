9b5383d6d4c5d8e4eb8c399e46104df6002c0f81
Kevin Washington <kevw12188@gmail.com>
2015-05-26 20:10:16 -0400

    fixed issue issue locally preventing all options from the schuduler showing up in the email

--- public/app/reEngagementForMeeting.js
+++ public/app/reEngagementForMeeting.js
@@ -30,9 +30,9 @@ angular.module('app')
 
         $scope.sendOptions = function (companyName, option1Info, option2Info, option3Info)
         {
-            randomString = Math.random().toString(36).substring(7);
+            var randomString = Math.random().toString(36).substring(7);
 
-            if(option3Info.day != null)
+            if(option3Info.day != undefined)
             {
                 sendMessage =
                     'I have these times available: '+
@@ -40,18 +40,18 @@ angular.module('app')
                     'Option2: ' + option2Info.day + ' @ ' + option2Info.time +
                     'Option3: ' + option3Info.day + ' @ ' + option3Info.time;
             }
-            if(option2Info.day == null)
-            {
-                sendMessage =
-                    'I have these times available: '+
-                    'Option1: ' + option1Info.day + ' @ ' + option1Info.time;
-            }
-            else
+            if(option3Info.day == undefined && option2Info.day != undefined)
             {
                 sendMessage =
                     'I have these times available: '+
                     'Option1: ' + option1Info.day + ' @ ' + option1Info.time +
                     'Option2: ' + option2Info.day + ' @ ' + option2Info.time;
+            }
+            if(option3Info.day == undefined && option2Info.day == undefined && option1Info.day != undefined)
+            {
+                sendMessage =
+                    'I have these times available: '+
+                    'Option1: ' + option1Info.day + ' @ ' + option1Info.time;
             }
 
             var data = {
