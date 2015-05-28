ad789c12cff5b98f4629fa0144ee97935f2f7df6
Kevin Washington <kevw12188@gmail.com>
2015-05-28 17:11:18 -0400

    added growthValley info to bodyOfWork page

--- app/views/publicPages/bodyOfWork.php
+++ app/views/publicPages/bodyOfWork.php
@@ -98,56 +98,59 @@
             <div class="bodyOfWorkProjectInput">
                 <div class="bodyOfWorkProjectInputGroup">
                     <img class="img-responsive img-circle center-block" src="http://placehold.it/80">
-                    <h3>Management</h3>
-                    <p>People</p>
-                    <p>Processes</p>
-                    <p>Revenue</p>
-                    <p>Business Data & Intelligence</p>
+                    <h3>Product Research</h3>
+                    <p>Potential Customer Research</p>
+                    <p>Preliminary Market Research</p>
+                    <p>Missing Need Identification</p>
+                    <p>Competitor Research</p>
                 </div>
                 <div class="bodyOfWorkProjectInputGroup">
                     <img class="img-responsive img-circle center-block" src="http://placehold.it/80">
-                    <h3>Lead Generation</h3>
-                    <p>Email Marketing</p>
-                    <p>Direct Interaction Marketing</p>
-                    <p>Paid Advertising</p>
-                    <p>Engagement Content Development</p>
-                    <p>Content Promotion via Social Media</p>
-                    <p>OnSite SEO Optimization</p>
+                    <h3>Product Strategy</h3>
+                    <p>Competitor Solution Gap</p>
+                    <p>Beach Head Marketing Strategy</p>
+                    <p>Branding</p>
+                    <p>Business Model / Revenue Generation Strategy</p>
+                    <p>Technology Selection</p>
                 </div>
                 <div class="bodyOfWorkProjectInputGroup">
                     <img class="img-responsive img-circle center-block" src="http://placehold.it/80">
-                    <h3>Digital Sales</h3>
-                    <p>Landing Page Development, Promotion, & Optimization</p>
-                    <p>Acquisition Process Optimization</p>
-                    <p>Optimization of User Experience</p>
-                    <p>Product Demonstration Promotion & Consulting</p>
-                    <p>New User OnBoarding Process Optimization</p>
+                    <h3>Product Development</h3>
+                    <p>Ruby on Rails Project Management</p>
+                    <p>Cover Design Management</p>
+                    <p>U/X Design Management</p>
+                    <p>Responsive Design</p>
+                    <p>CopyWriting</p>
+                    <p>Testing & Quality Assurance</p>
                 </div>
             </div>
 
             <div class="bodyOfWorkProjectInfo row">
                 <div class="bodyOfWorkProjectInfoTitle col-lg-12">
                     <img class="img-responsive img-circle center-block" src="http://placehold.it/80">
-                    <h3>A little Background</h3>
+                    <h3>About the GV Project</h3>
                 </div>
                 <div class="bodyOfWorkProjectInfoBody col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
                     <p>
-                        karmaCRM is a SaaS based business that offers Customer Relationship Management solutions to small and medium sized businesses.
-                        I was initially contracted to karmaCRM to provide SEO services.
-                        However after learning more about their business model and spotting some under-performing processes, I worked with them to accomplish much more.
-                        Eventually working my way up to the role of Director of Growth.
+                        This was initially a consulting project that transformed into a full blown software development contract.
+                        The owner looked to solve the niche problems that new web based entrepreneurs experience.
+                        These markets include Developers who don't know how to market, marketers who don't anything about code,
+                        and stakeholders who know neither.
+                        Working with the owner we came up with solutions that included innovative learning solutions like;
+                        Facilitating one on one training, unique courses that allowed interaction with the course provider,
+                        and vetted resources from industry leaders.
                     </p>
                 </div>
             </div>
             <div class="bodyOfWorkProjectViewWorkExamples">
-                <button class="btn btn-warning btn-lg" ng-click="showProjectWorkExample('karmaCRM')">Some of my karmaCRM work</button>
+                <button class="btn btn-warning btn-lg" ng-click="showProjectWorkExample('growthValley')">My GrowthValley Work</button>
             </div>
-            <div ng-show="showingProjectWorkExample == 'karmaCRM'" class="bodyOfWorkProjectWorkExamples col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+            <div ng-show="showingProjectWorkExample == 'growthValley'" class="bodyOfWorkProjectWorkExamples col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
                 <p>
-                    While with <a href="http://karmacrm.com" target="_blank">karmaCRM</a>
-                    I lead a growth strategy that had some amazing results.
-                    We increased our free trial conversions, revenue, and reduced the negative effects of our attrition.
-                    <a href="/karma-crm-stats" target="_blank">See how we did it.</a>
+                    The project has since completed and is now a full alpha-ready SaaS product.
+                    The owner asked me to stay on as a co-founder to which I had to decline due to time conflicts.
+                    You can see the full project <a href="http://growthvalley.herokuapp.com/#" target="_blank">here</a>.
+                    Try to give it a minute to load as the owner is looking to keep cost down allocating very little dynos via Heroku hosting platform.
                 </p>
                 <a ng-click="hideProjectWorkExample()">Hide</a>
             </div>
