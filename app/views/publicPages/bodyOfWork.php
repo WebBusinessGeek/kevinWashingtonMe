ba6119e752d00509e567860fd82264cf32a53180
Kevin Washington <kevw12188@gmail.com>
2015-05-28 13:47:39 -0400

    started structure of body of work page

--- app/views/publicPages/bodyOfWork.php
+++ app/views/publicPages/bodyOfWork.php
@@ -1,5 +1,34 @@
 <div id="bodyOfWorkPageContainer" class="text-center" ng-controller="bodyOfWorkController">
+
+    <div id="bodyOfWorkPageMainHeader">
+        <h1 id="bodyOfWorkPageTitle">A Few of My Projects.</h1>
+        <p id="bodyOfWorkPageSubTitle">
+            Short compendium of projects I have had the pleasure of working on.
+            Either marketing, web development or both.
+        </p>
+    </div>
+
     <div>
-        {{test}}
+        <p>karma section</p>
+    </div>
+
+    <div>
+        <p>GrowthValley section</p>
+    </div>
+
+    <div>
+        <p>SmartHealthyWoman section</p>
+    </div>
+
+    <div>
+        <p>NomadYogi Section</p>
+    </div>
+
+    <div>
+        <p>Family law center section</p>
+    </div>
+
+    <div>
+        <p>Sacramento employment lawyers section</p>
     </div>
 </div>
