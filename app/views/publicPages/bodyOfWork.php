0b4ff6cfcb42bdea4a2b6baef6f947bb0fd1ec6e
Kevin Washington <kevw12188@gmail.com>
2015-05-28 14:07:45 -0400

    set up architecture for page

--- app/views/publicPages/bodyOfWork.php
+++ app/views/publicPages/bodyOfWork.php
@@ -4,31 +4,181 @@
         <h1 id="bodyOfWorkPageTitle">A Few of My Projects.</h1>
         <p id="bodyOfWorkPageSubTitle">
             Short compendium of projects I have had the pleasure of working on.
-            Either marketing, web development or both.
+            Either marketing, product development or both.
         </p>
     </div>
 
-    <div>
-        <p>karma section</p>
+    <div class="bodyOfWorkProjectContainer">
+        <div class="bodyOfWorkProjectHeader">
+            <div class="bodyOfWorkProjectTitle">
+                <h3>Director Of Growth</h3>
+            </div>
+            <div class="bodyOfWorkProjectLogo">
+                <img class="img-circle img-responsive center-block" src="http://placehold.it/150">
+            </div>
+            <div class="bodyOfWorkProjectCompany">
+                <h1>karmaCRM</h1>
+            </div>
+        </div>
+
+        <div class="bodyOfWorkProjectBody">
+            <div class="bodyOfWorkProjectInput">
+
+            </div>
+            <div class="bodyOfWorkProjectInfo">
+
+            </div>
+            <div class="bodyOfWorkProjectViewWorkExamples">
+
+            </div>
+            <div class="bodyOfWorkProjectWorkExamples">
+
+            </div>
+        </div>
     </div>
 
-    <div>
-        <p>GrowthValley section</p>
+    <div class="bodyOfWorkProjectContainer">
+        <div class="bodyOfWorkProjectHeader">
+            <div class="bodyOfWorkProjectTitle">
+                <h3>Product Manager</h3>
+            </div>
+            <div class="bodyOfWorkProjectLogo">
+                <img class="img-circle img-responsive center-block" src="http://placehold.it/150">
+            </div>
+            <div class="bodyOfWorkProjectCompany">
+                <h1>GrowthValley</h1>
+            </div>
+        </div>
+
+        <div class="bodyOfWorkProjectBody">
+            <div class="bodyOfWorkProjectInput">
+
+            </div>
+            <div class="bodyOfWorkProjectInfo">
+
+            </div>
+            <div class="bodyOfWorkProjectViewWorkExamples">
+
+            </div>
+            <div class="bodyOfWorkProjectWorkExamples">
+
+            </div>
+        </div>
     </div>
 
-    <div>
-        <p>SmartHealthyWoman section</p>
+    <div class="bodyOfWorkProjectContainer">
+        <div class="bodyOfWorkProjectHeader">
+            <div class="bodyOfWorkProjectTitle">
+                <h3>SEO Consultant</h3>
+            </div>
+            <div class="bodyOfWorkProjectLogo">
+                <img class="img-circle img-responsive center-block" src="http://placehold.it/150">
+            </div>
+            <div class="bodyOfWorkProjectCompany">
+                <h1>SmartHealthyWomen</h1>
+            </div>
+        </div>
+
+        <div class="bodyOfWorkProjectBody">
+            <div class="bodyOfWorkProjectInput">
+
+            </div>
+            <div class="bodyOfWorkProjectInfo">
+
+            </div>
+            <div class="bodyOfWorkProjectViewWorkExamples">
+
+            </div>
+            <div class="bodyOfWorkProjectWorkExamples">
+
+            </div>
+        </div>
     </div>
 
-    <div>
-        <p>NomadYogi Section</p>
+    <div class="bodyOfWorkProjectContainer">
+        <div class="bodyOfWorkProjectHeader">
+            <div class="bodyOfWorkProjectTitle">
+                <h3>Digital Marketing Consultant</h3>
+            </div>
+            <div class="bodyOfWorkProjectLogo">
+                <img class="img-circle img-responsive center-block" src="http://placehold.it/150">
+            </div>
+            <div class="bodyOfWorkProjectCompany">
+                <h1>NomadYogi</h1>
+            </div>
+        </div>
+
+        <div class="bodyOfWorkProjectBody">
+            <div class="bodyOfWorkProjectInput">
+
+            </div>
+            <div class="bodyOfWorkProjectInfo">
+
+            </div>
+            <div class="bodyOfWorkProjectViewWorkExamples">
+
+            </div>
+            <div class="bodyOfWorkProjectWorkExamples">
+
+            </div>
+        </div>
     </div>
 
-    <div>
-        <p>Family law center section</p>
+    <div class="bodyOfWorkProjectContainer">
+        <div class="bodyOfWorkProjectHeader">
+            <div class="bodyOfWorkProjectTitle">
+                <h3>Advertising Manager</h3>
+            </div>
+            <div class="bodyOfWorkProjectLogo">
+                <img class="img-circle img-responsive center-block" src="http://placehold.it/150">
+            </div>
+            <div class="bodyOfWorkProjectCompany">
+                <h1>Family Law Center</h1>
+            </div>
+        </div>
+
+        <div class="bodyOfWorkProjectBody">
+            <div class="bodyOfWorkProjectInput">
+
+            </div>
+            <div class="bodyOfWorkProjectInfo">
+
+            </div>
+            <div class="bodyOfWorkProjectViewWorkExamples">
+
+            </div>
+            <div class="bodyOfWorkProjectWorkExamples">
+
+            </div>
+        </div>
     </div>
 
-    <div>
-        <p>Sacramento employment lawyers section</p>
+    <div class="bodyOfWorkProjectContainer">
+        <div class="bodyOfWorkProjectHeader">
+            <div class="bodyOfWorkProjectTitle">
+                <h3>Digital Marketing Consultant</h3>
+            </div>
+            <div class="bodyOfWorkProjectLogo">
+                <img class="img-circle img-responsive center-block" src="http://placehold.it/150">
+            </div>
+            <div class="bodyOfWorkProjectCompany">
+                <h1>Sacramento Family Law</h1>
+            </div>
+        </div>
+
+        <div class="bodyOfWorkProjectBody">
+            <div class="bodyOfWorkProjectInput">
+
+            </div>
+            <div class="bodyOfWorkProjectInfo">
+
+            </div>
+            <div class="bodyOfWorkProjectViewWorkExamples">
+
+            </div>
+            <div class="bodyOfWorkProjectWorkExamples">
+
+            </div>
+        </div>
     </div>
 </div>
