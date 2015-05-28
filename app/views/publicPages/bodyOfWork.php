fdfb4af51d5d03d0e64f7faae641a677da15db15
Kevin Washington <kevw12188@gmail.com>
2015-05-28 17:59:14 -0400

    added smartHealthyWomen content to bodyOfWork page

--- app/views/publicPages/bodyOfWork.php
+++ app/views/publicPages/bodyOfWork.php
@@ -73,7 +73,7 @@
                 <p>
                     While with <a href="http://karmacrm.com" target="_blank">karmaCRM</a>
                     I lead a growth strategy that had some amazing results.
-                    We increased our free trial conversions, revenue, and reduced the negative effects of our attrition.
+                    We increased our free trial conversions, revenue, and reduced the negative effects of our attrition by huge percentages.
                     <a href="/karma-crm-stats" target="_blank">See how we did it.</a>
                 </p>
                 <a ng-click="hideProjectWorkExample()">Hide</a>
@@ -174,56 +174,37 @@
             <div class="bodyOfWorkProjectInput">
                 <div class="bodyOfWorkProjectInputGroup">
                     <img class="img-responsive img-circle center-block" src="http://placehold.it/80">
-                    <h3>Management</h3>
-                    <p>People</p>
-                    <p>Processes</p>
-                    <p>Revenue</p>
-                    <p>Business Data & Intelligence</p>
-                </div>
-                <div class="bodyOfWorkProjectInputGroup">
-                    <img class="img-responsive img-circle center-block" src="http://placehold.it/80">
-                    <h3>Lead Generation</h3>
-                    <p>Email Marketing</p>
-                    <p>Direct Interaction Marketing</p>
-                    <p>Paid Advertising</p>
-                    <p>Engagement Content Development</p>
-                    <p>Content Promotion via Social Media</p>
-                    <p>OnSite SEO Optimization</p>
-                </div>
-                <div class="bodyOfWorkProjectInputGroup">
-                    <img class="img-responsive img-circle center-block" src="http://placehold.it/80">
-                    <h3>Digital Sales</h3>
-                    <p>Landing Page Development, Promotion, & Optimization</p>
-                    <p>Acquisition Process Optimization</p>
-                    <p>Optimization of User Experience</p>
-                    <p>Product Demonstration Promotion & Consulting</p>
-                    <p>New User OnBoarding Process Optimization</p>
+                    <h3>SEO Consulting</h3>
+                    <p>Audience Interest Research</p>
+                    <p>Keyword Research</p>
+                    <p>Competition Research Process & Tools</p>
+                    <p>Content Strategy</p>
+                    <p>Search Engine Algorithm Basics</p>
+                    <p>Content Promotion</p>
                 </div>
             </div>
 
             <div class="bodyOfWorkProjectInfo row">
                 <div class="bodyOfWorkProjectInfoTitle col-lg-12">
                     <img class="img-responsive img-circle center-block" src="http://placehold.it/80">
-                    <h3>A little Background</h3>
+                    <h3>SEO Training @ SmartHealthWomen</h3>
                 </div>
                 <div class="bodyOfWorkProjectInfoBody col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
                     <p>
-                        karmaCRM is a SaaS based business that offers Customer Relationship Management solutions to small and medium sized businesses.
-                        I was initially contracted to karmaCRM to provide SEO services.
-                        However after learning more about their business model and spotting some under-performing processes, I worked with them to accomplish much more.
-                        Eventually working my way up to the role of Director of Growth.
+                        SmartHealthyWomen is now a successful Austrailian based blog targeting women and their interests.
+                        A few years ago when starting the blog the owner reached out to me for help in marketing her content.
+                        She is one of the most efficient and engaged business professionals I have worked with and wanted to learn how to maintain an SEO presence on her own.
+                        So she contracted me to help her learn SEO.
                     </p>
                 </div>
             </div>
             <div class="bodyOfWorkProjectViewWorkExamples">
-                <button class="btn btn-warning btn-lg" ng-click="showProjectWorkExample('karmaCRM')">Some of my karmaCRM work</button>
+                <button class="btn btn-warning btn-lg" ng-click="showProjectWorkExample('smartHealthyWomen')">My Time with SHW</button>
             </div>
-            <div ng-show="showingProjectWorkExample == 'karmaCRM'" class="bodyOfWorkProjectWorkExamples col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+            <div ng-show="showingProjectWorkExample == 'smartHealthyWomen'" class="bodyOfWorkProjectWorkExamples col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
                 <p>
-                    While with <a href="http://karmacrm.com" target="_blank">karmaCRM</a>
-                    I lead a growth strategy that had some amazing results.
-                    We increased our free trial conversions, revenue, and reduced the negative effects of our attrition.
-                    <a href="/karma-crm-stats" target="_blank">See how we did it.</a>
+                    The project consisted of training so there are few deliverables I can link to.
+                    However feel free to check out how Estelle is doing by visiting her <a href="http://www.smarthealthywomen.com/" target="_blank">blog</a> and <a href="https://www.facebook.com/SmartHealthyWomen?_rdr" target="_blank">facebook page</a>.
                 </p>
                 <a ng-click="hideProjectWorkExample()">Hide</a>
             </div>
